c8040cfe3efaa8ddd0d89f035bfdcbf7bd257426
Angela Lindow <angelalindow@gmail.com>
2016-05-30 14:48:26 -0600

    it0

--- lib/server.rb
+++ lib/server.rb
@@ -2,14 +2,11 @@ require 'socket'
 tcp_server = TCPServer.new(9292)
 client = tcp_server.accept
 
-puts "Ready for a request"
 request_lines = []
-while line = client.gets and !line.chomp.empty?
+while line = client.gets
   request_lines << line.chomp
-end
 
-puts "Sending response."
-response = "<pre>" + "Hello World(#{request_lines.count})" + "</pre>"
+response = "<pre>" + "hello world(#{request_lines.count})" + "</pre>"
 output = "<html><head></head><body>#{response}</body></html>"
 headers = ["http/1.1 200 ok",
           "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
@@ -18,14 +15,4 @@ headers = ["http/1.1 200 ok",
           "content-length: #{output.length}\r\n\r\n"].join("\r\n")
 client.puts headers
 client.puts output
-
-
-# require 'socket'
-#
-# class Server
-#
-#   def initialize
-#     @tcp_server = TCPServer.new(9292)
-#     @client = tcp_server.accept
-#   end
-# end
+end
--- test/server_test.rb
+++ test/server_test.rb
@@ -1,10 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'rake/testtask'
+# require 'rake/testtask'
 
 class SeverTest < Minitest::test
 
   def test_listens_to_port_9292
-    
+
 
 end
