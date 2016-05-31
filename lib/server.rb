cb06467a47937f0b309329a1fc006593dc188051
Angela Lindow <angelalindow@gmail.com>
2016-05-30 18:10:58 -0600

    iteration 1

--- lib/server.rb
+++ lib/server.rb
@@ -11,15 +11,23 @@ while line = client.gets and !line.chomp.empty?
   request_lines << line.chomp
 end
 
+if client.gets == "/"
 response = "<pre>" + request_lines.join("\n") + "</pre>"
 output = "<html><head></head><body>#{response}</body></html>"
-headers = ["http/1.1 200 ok",
-          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-          "server: ruby",
-          "content-type: text/html; charset=iso-8859-1",
-          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-# client.puts headers
-# client.puts output
+headers = ["Verb: POST",
+          "Path: /",
+          "Protocol: HTTP/1.1",
+          "Host: 127.0.0.1",
+          "Port: 9292",
+          "Origin: 127.0.0.1",
+          "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"]
+
+
+client.puts headers
 client.puts "Hello World #{counter}"
 client.close
+else
+  client.puts "yeah"
+  client.close
+end
 end
