2522ea3aa63f8ba2210fb82878bf49ba8ac55d91
Angela Lindow <angelalindow@gmail.com>
2016-05-31 17:56:39 -0600

    refactored Response

--- lib/response.rb
+++ lib/response.rb
@@ -3,48 +3,43 @@
 require "./lib/parser"
 
 class Response
-  attr_reader :parser
+  attr_reader :parser,
+              :request_lines
 
-  def initialize
+  def initialize(request_lines)
     @parser = Parser.new
+    @request_lines = request_lines
   end
 
-  # def user_path(path)
-  #   case path
-  #   when parser.get_path == '/'
-  #     send_response
-  #   else
-  #     "Hello"
-  #   end
-  # end
   def date_and_time
     Time.now.strftime("%m:%M%p on %A, %B %e, %Y ")
   end
 
-  def send_response(client,hello_counter,counter,request_lines)
-    response = "<pre>" + request_lines.join("\n") + "</pre>"
-    output = "<html><head></head><body>#{response}</body></html>"
-    headers = ["Verb: #{parser.get_verb(request_lines)}",
+  def path_controller(client,hello_counter,counter,request_lines)
+      case parser.get_path(request_lines)
+    when '/'
+      client.puts request_info(request_lines)
+    when '/hello'
+      client.puts "Hello World #{hello_counter}"
+    when '/datetime'
+      client.puts date_and_time
+    when '/shutdown'
+      client.puts "Total Requests: #{counter}"
+    else
+      client.puts "Check this guy out, he thinks #{parser.get_path(request_lines)} is a thing! It's not. Sorry."
+    end
+    client.close
+  end
+
+  def request_info(request_lines)
+    output = ["<pre>Verb: #{parser.get_verb(request_lines)}",
       "Path: #{parser.get_path(request_lines)}",
       "Protocol: #{parser.get_protocol(request_lines)}",
       "Host: #{parser.get_host(request_lines)}",
       "Port: #{parser.get_port(request_lines)}",
       "Origin: #{parser.get_host(request_lines)}",
-      "#{parser.get_accept(request_lines)}"]
-      case
-      when parser.get_path(request_lines) == '/'
-        client.puts headers
-      when parser.get_path(request_lines) == '/hello'
-        client.puts "Hello World #{hello_counter}"
-      when parser.get_path(request_lines) == '/datetime'
-        client.puts date_and_time
-      when parser.get_path(request_lines) == '/shutdown'
-        client.puts "Total Requests: #{counter}"
-        
-      else
-        client.puts "Check this guy out, he thinks #{parser.get_path(request_lines)} is a thing! It's not. Sorry."
-      end
-      client.close
-    end
-
+      "#{parser.get_accept(request_lines)}</pre>"]
+    output
   end
+
+end
--- test/server_test.rb
+++ test/server_test.rb
@@ -1,20 +1,20 @@
-# require 'minitest/autorun'
-# require 'minitest/pride'
-# require 'faraday'
-# require './lib/server'
-# require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'faraday'
+require './lib/server'
+require 'pry'
 #
-# class SeverTest < Minitest::test
-#
+class SeverTest < Minitest::Test
+
 #   def setup
 #     @connection - Faraday.new('http://127.0.0.1:9292')
 #     faraday.request :url_encoded
 #     faraday.adapter Faraday.default_adapter
 #   end
 #
-#   def test_server_responds_to_request
-#     response = Faraday.get('http://127.0.0.1:9292')
-#     assert_equal nil,response.inspect
-#   end
-#
-# end
+  def test_server_responds_to_request
+    response = Faraday.new(:url =>'http://127.0.0.1:9292')
+    assert_equal nil,response.get("/")
+  end
+
+end
